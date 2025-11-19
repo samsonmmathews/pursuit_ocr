6ea75c276dc6985695a25c63fb65dec688f62f5c
Chenxi Lin <lin.chenxi14@gmail.com>
2025-11-19 13:59:32 -0500

    Edit banner backgorund image

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import './banner.css'
 
-const Banner = ({image}) => {
+const Banner = ({image, title}) => {
 
    
 
@@ -9,6 +9,7 @@ const Banner = ({image}) => {
         <section aria-hidden="true" id="banner-container" style={{
             backgroundImage: `url(${image})`
         }}>
+            {title ? <h2 id='banner-heading'>{title}</h2> : ""}
         </section>
     );
 }
---
+++ src/components/OurSpace/index.jsx
@@ -0,0 +1,69 @@
+import Banner from "../Banner/Banner";
+
+
+const OurSpace = () => {
+    return(
+        <main>
+            <h2>Our Space</h2>
+            <Banner title={"The Track"} image={"/background2.svg"} />
+            <section id='home2'>
+                <div>
+                    <p>The ball pit at OCR Pursuit is no ordinary playground feature — it’s a massive, four-and-a-half-foot-deep sea of over 100,000 balls that feels more like an endurance challenge than a casual dip. The moment you jump in, you’re swallowed up by the colourful chaos, struggling to stay afloat as you wade through the endless layers. It’s fun, disorienting, and just a little terrifying — especially when you realize how hard it is to get out. Definitely worth trying once, but keep your phone (and maybe your shoes) far away unless you want to lose them to the depths forever.</p>
+                    <ul>
+                        <li>You might lose your shoes, but you’ll gain an unforgettable story!</li>
+                        <li>Feels like swimming and sinking at the same time!</li>
+                        <li>A hilarious full-body workout you never saw coming!</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <Banner title={"The Ball Pit"} image={"/Background.svg"}/>
+            <section id='home2'>
+                <div>
+                    <p>The ball pit at OCR Pursuit is no ordinary playground feature — it’s a massive, four-and-a-half-foot-deep sea of over 100,000 balls that feels more like an endurance challenge than a casual dip. The moment you jump in, you’re swallowed up by the colourful chaos, struggling to stay afloat as you wade through the endless layers. It’s fun, disorienting, and just a little terrifying — especially when you realize how hard it is to get out. Definitely worth trying once, but keep your phone (and maybe your shoes) far away unless you want to lose them to the depths forever.</p>
+                    <ul>
+                        <li>You might lose your shoes, but you’ll gain an unforgettable story!</li>
+                        <li>Feels like swimming and sinking at the same time!</li>
+                        <li>A hilarious full-body workout you never saw coming!</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <Banner title={"The Cove Wall"} image={"/ourSpace_banner.svg"} />
+            <section id='home2'>
+                <div>
+                    <p>The Cove Wall at Pursuit OCR is a thrilling challenge that feels straight out of an action movie. With its towering curved ramp and glowing neon cubes floating overhead, it’s designed to test your speed, strength, and determination. You’ll feel the rush as you sprint toward the wall, push off, and try to reach the top a perfect mix of athleticism and adrenaline. The vibrant lights and bold design make it not only an exciting obstacle to conquer but also a spot that practically begs for a victory photo once you’ve made it to the top.</p>
+                    <ul>
+                        <li>Neon lights and cube art make it feel like you’ve stepped into a sci-fi adventure</li>
+                        <li>Whether you conquer it on the first try or the fifth, you’ll leave grinning—and maybe a little breathless.</li>
+                        <li>Great for grip, balance, and courage (and an epic photo opportunity!)</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <Banner title={"The Axe Throw"} image={"/ourSpace_banner.svg"} />
+            <section id='home2'>
+                <div>
+                    <p>The ball pit at OCR Pursuit is no ordinary playground feature — it’s a massive, four-and-a-half-foot-deep sea of over 100,000 balls that feels more like an endurance challenge than a casual dip. The moment you jump in, you’re swallowed up by the colourful chaos, struggling to stay afloat as you wade through the endless layers. It’s fun, disorienting, and just a little terrifying — especially when you realize how hard it is to get out. Definitely worth trying once, but keep your phone (and maybe your shoes) far away unless you want to lose them to the depths forever.</p>
+                    <ul>
+                        <li>You might lose your shoes, but you’ll gain an unforgettable story!</li>
+                        <li>Feels like swimming and sinking at the same time!</li>
+                        <li>A hilarious full-body workout you never saw coming!</li>
+                    </ul>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </section>
+            <Banner title={"Tag Zone"} image={"/ourSpace_banner.svg"} />
+        </main>
+    );
+}
+
+export default OurSpace;
